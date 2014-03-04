0b1473330b59664e9a705d0e4c9a33c1dca9e47b
Somshubra Majumdar <titu1994@gmail.com>
2014-03-04 21:59:56 +0530

    Finished

---
+++ LongestCommonSubsequence/src/LongestCommonSubsequence.c
@@ -0,0 +1,71 @@
+/*
+ ============================================================================
+ Name        : LongestCommonSubsequence.c
+ Author      : Somshubra Majumdar
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 100
+
+void longestCommonSubsequence(char *x,char *y,char *z);
+
+int main(void) {
+	char x[SIZE], y[SIZE], z[SIZE];
+
+	setbuf(stdout, NULL);
+
+	printf("Enter 2 strings of characters\n");
+	scanf("%s%s", x, y);
+
+	longestCommonSubsequence(x,y,z);
+
+	printf("The longest common subsequence is :\n%s", z);
+
+	return EXIT_SUCCESS;
+}
+
+void longestCommonSubsequence(char x[SIZE],char y[SIZE],char z[SIZE]){
+	int i,n,m,j,k;
+	int T[100][100] = {{0}};
+
+	m = strlen(x);
+	n = strlen(y);
+
+	for(i = 1; i <= m; i++){
+		for(j = 1; j <= n; j++){
+			if(x[i-1] == y[j-1])
+				T[i][j] = T[i-1][j-1] + 1;
+			else {
+				if(T[i-1][j] > T[i][j])
+					T[i][j] = T[i-1][j];
+				else
+					T[i][j] = T[i][j-1];
+			}
+		}
+	}
+
+	i = m;
+	j = n;
+	k = 0;
+
+	while(i > 0 && j > 0){
+		if(x[i-1] == y[j-1]) {
+			z[k++] = x[i-1];
+			i--;
+		}
+		else if(T[i-1][j] > T[i][j-1])
+			i--;
+		else
+			j--;
+	}
+
+	z[k] = '\0';
+	strrev(z);
+}
